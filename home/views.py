1e9ed6e7262bece3126642f383f635d8b92e8718
Saurav <sp.gharti@gmail.com>
2018-06-20 16:28:37 +0000

    add package Misago

--- home/views.py
+++ home/views.py
@@ -8,6 +8,7 @@ from home.models import CustomText, HomePage
 def home(request):
     packages = [
 	{'name':'Misago', 'url': 'http://pypi.python.org/pypi/Misago/0.14.0'},
+	{'name':'Misago', 'url': 'http://pypi.python.org/pypi/Misago/0.14.0'},
 	{'name':'django-social-auth', 'url': 'http://pypi.python.org/pypi/django-social-auth/0.7.28'},
 	{'name':'django-social-auth', 'url': 'http://pypi.python.org/pypi/django-social-auth/0.7.28'},
     ]
